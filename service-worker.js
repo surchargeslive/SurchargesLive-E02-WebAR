0daed03014c43522a534916aecc481542e18d985
Jean-François Garreau - Binomed <jean.francois.garreau@gmail.com>
2019-06-26 17:42:09 +0200

    files for service worker

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,17 @@
 console.log('Service worker ok =D');
 
 var cacheAppShellStatic = [
-  '/',
   '/offline.html',
-  '/libs/aframe-ar.js',
-  '/libs/aframe.min.js',
-  '/models/cactus_A.stl.gltf',
-  '/models/cactus_B.stl.gltf',
-  '/models/cactus_C.stl.gltf',
-  '/models/cactus_D.stl.gltf',
-  '/models/cactus_E.stl.gltf',
-  '/models/cactus_F.stl.gltf',
-  '/models/Dino.stl.gltf',
+  '/node_modules/aframe/dist/aframe-v0.9.2.js',
+  '/node_modules/ar.js/aframe/build/aframe-ar.js',
+  '/node_modules/ar.js/data/data/camera_para.dat',
+  '/models/cactus_A.gltf',
+  '/models/cactus_B.gltf',
+  '/models/cactus_C.gltf',
+  '/models/cactus_D.gltf',
+  '/models/cactus_E.gltf',
+  '/models/cactus_F.gltf',
+  '/models/Dino.gltf',
   '/assets/pattern-offline.patt',
   '/main.js',
 ];
@@ -36,11 +36,17 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function(error) {
-      return caches.open('cache-static').then(function(cache) {
-        return cache.match('offline.html');
-      });
-    })
-  );
+  if (event.request.method === 'GET'){
+    event.respondWith(
+      caches.match(event.request).then((response) =>
+      response ||
+      fetch(event.request)
+        .catch(() => caches.match(new Request('offline.html')).then((resp)=> {
+            console.log(resp);
+            return resp;
+          })
+        )
+      )
+    );
+  }
 });
